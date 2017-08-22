ac2a8b3d5f7f01d36e84a14d77d0e867d2ac61ed
mido <mido.ghanim@gmail.com>
2017-08-22 16:47:09 -0700

    solved some problem

--- arraysPractice.js
+++ arraysPractice.js
@@ -28,7 +28,7 @@ function last(arr){
 
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
-function looper() {
+function looper(family) {
   for(var i = 0; i < family.length; i++) {
     alert (family[i]);
   }
